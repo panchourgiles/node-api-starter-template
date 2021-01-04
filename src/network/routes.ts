466670f35a8e61549d8fc32527038926231d8509
Pancho Urgiles <panchourgiles@gmail.com>
2021-01-03 22:56:43 -0500

    Add message in / route

--- src/network/routes.ts
+++ src/network/routes.ts
@@ -1,8 +1,12 @@
 import userRouter from '@components/user/network';
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 export default (): Router => {
   const router: Router = Router();
+
+  router.get('/', (_req: Request, res: Response) => {
+    return res.json({ message: 'Api is ready' });
+  });
 
   router.use('/users', userRouter());
   return router;
