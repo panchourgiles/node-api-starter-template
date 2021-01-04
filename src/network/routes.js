87cc497d6c86abb8c647b0e855147c75deb98d67
Pancho Urgiles <panchourgiles@gmail.com>
2021-01-03 22:40:58 -0500

    Add message in / route

--- src/network/routes.js
+++ src/network/routes.js
@@ -4,6 +4,10 @@ import { Router } from 'express';
 export default () => {
   const router = Router();
 
+  router.get('/', (_req, res) => {
+    return res.json({ message: 'Api is ready' });
+  });
+
   router.use('/users', userRouter());
   return router;
 };
