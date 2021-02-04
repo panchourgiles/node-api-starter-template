348667f107770ae9193516625cddc6bd75f2a06c
Pancho Urgiles <panchourgiles@gmail.com>
2021-02-03 21:46:17 -0500

    Use response function in / endpoint

--- src/network/routes.js
+++ src/network/routes.js
@@ -1,11 +1,12 @@
 import userRouter from '@components/user/network';
+import response from '@network/response';
 import { Router } from 'express';
 
 export default () => {
   const router = Router();
 
   router.get('/', (_req, res) => {
-    return res.json({ message: 'Api is ready' });
+    return response(res, { message: 'Api is ready' }, 200);
   });
 
   router.use('/users', userRouter());
