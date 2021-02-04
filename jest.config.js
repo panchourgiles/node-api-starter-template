9ccae0dac1425ee9e49c889e482e0b6435495560
Pancho Urgiles <panchourgiles@gmail.com>
2021-02-03 21:43:15 -0500

    Install and configure eslint-plugin-jest and improve jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  verbose: true
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/index.js',
+    'src/loaders/mongoose.js'
+  ],
+  verbose: true,
+  collectCoverageFrom: ['src/**/*.js']
 };
